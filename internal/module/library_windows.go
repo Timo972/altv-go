5483a32ce9471a5e3d2538082da6d5db48c488df
shock <fastestmanonmars@gmail.com>
2021-08-05 00:07:35 +0200

    Fix library windows

--- internal/module/library_windows.go
+++ internal/module/library_windows.go
@@ -1,4 +1,5 @@
 // +build windows
+
 package module
 
 import (
@@ -7,10 +8,6 @@ import (
 )
 
 var dll = syscall.NewLazyDLL("go-module.dll")
-
-func GetLibrary() *syscall.LazyDLL {
-	return dll
-}
 
 func GetFunc(name string, a ...uintptr) {
 	proc := dll.NewProc(name)
