5b7301283880fcbfcb34cceb43e74e860da0f1b2
shock <fastestmanonmars@gmail.com>
2021-08-16 15:08:25 +0200

    Add command handler

---
+++ alt/cmd_handler.go
@@ -0,0 +1,31 @@
+package alt
+
+type Invoker struct {
+	commands map[string]Cmd
+}
+
+type Cmd interface {
+	Name() string
+	Execute(p *Player)
+}
+
+func (i Invoker) RegisterCmd(c Cmd) {
+	_, exists := i.commands[c.Name()]
+
+	if exists {
+		LogError("Command already registered.")
+		return
+	}
+
+	i.commands[c.Name()] = c
+}
+
+func (i Invoker) ExecuteCmd(p *Player, name string) {
+	cmd, exists := i.commands[name]
+
+	if !exists {
+		return
+	}
+
+	cmd.Execute(p)
+}
