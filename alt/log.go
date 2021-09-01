3250d7261696a8802345fa6c5ccd87e61070a785
Timo972 <beckmann.timo@icloud.com>
2021-09-01 20:34:31 +0200

    improve log function

--- alt/log.go
+++ alt/log.go
@@ -3,37 +3,66 @@ package alt
 // #include <stdlib.h>
 // #include "Module.h"
 import "C"
-import "unsafe"
+import (
+	"encoding/json"
+	"unsafe"
+)
 
-func LogInfo(msg string) {
+func stringify(args []interface{}) string {
+	str := ""
+	for i, arg := range args {
+		if i > 0 {
+			str += " "
+		}
+		msg, ok := arg.(string)
+		if ok {
+			str += msg
+			continue
+		}
+		data, err := json.Marshal(arg)
+		if err != nil {
+			str += "{}"
+		} else {
+			str += string(data)
+		}
+	}
+	return str
+}
+
+func LogInfo(args...interface{}) {
+	msg := stringify(args)
 	cstr := C.CString(msg)
 	defer C.free(unsafe.Pointer(cstr))
 
 	C.core_log_info(cstr)
 }
 
-func LogDebug(msg string) {
+func LogDebug(args ...interface{}) {
+	msg := stringify(args)
 	cstr := C.CString(msg)
 	defer C.free(unsafe.Pointer(cstr))
 
 	C.core_log_debug(cstr)
 }
 
-func LogWarning(msg string) {
+func LogWarning(args ...interface{}) {
+	msg := stringify(args)
 	cstr := C.CString(msg)
 	defer C.free(unsafe.Pointer(cstr))
 
 	C.core_log_warning(cstr)
 }
 
-func LogError(msg string) {
+func LogError(args ...interface{}) {
+	msg := stringify(args)
 	cstr := C.CString(msg)
 	defer C.free(unsafe.Pointer(cstr))
 
 	C.core_log_error(cstr)
 }
 
-func LogColored(msg string) {
+func LogColored(args ...interface{}) {
+	msg := stringify(args)
 	cstr := C.CString(msg)
 	defer C.free(unsafe.Pointer(cstr))
 
