b0959406853d4636c08b6ed80558c94a765b86b6
Timo972 <44401485+Timo972@users.noreply.github.com>
2022-04-12 15:13:07 +0200

    feat: benchmark utility

---
+++ utils/bench.go
@@ -0,0 +1,32 @@
+package utils
+
+import "time"
+import "github.com/shockdev04/altv-go-pkg/alt"
+
+type Benchmark struct {
+	Name    string
+	Begin   time.Time
+	End     time.Time
+	Elapsed time.Duration
+}
+
+func NewBenchmark(name string) *Benchmark {
+	return &Benchmark{
+		Name: name,
+	}
+}
+
+func (b *Benchmark) Start() {
+	b.Begin = time.Now()
+}
+
+func (b *Benchmark) Stop() {
+	b.End = time.Now()
+
+	b.Elapsed = b.End.Sub(b.Begin)
+}
+
+func (b *Benchmark) Print() {
+	ms := b.Elapsed.Milliseconds()
+	alt.LogColored("~ly~[BENCH] ", b.Name, " took: ", ms, "ms")
+}
