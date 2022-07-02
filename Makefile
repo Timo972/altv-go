8a553c8984e7ced07528a73656c3fb069ea09dbb
Timo972 <44401485+Timo972@users.noreply.github.com>
2022-07-02 16:13:50 +0200

    feat: grpc building :tada:

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 proto:
-	protoc --go_out=.\ .\runtime\src\mvalue.proto
+	protoc --go_out=.\ .\runtime\src\mvalue.proto --proto_path=.\runtime
 
 test:
 	cd api/alt && go test -v
--- runtime/build.bat
+++ runtime/build.bat
@@ -7,6 +7,6 @@ IF NOT EXIST bin\ (
     mkdir bin
 )
 pushd build
-cmake -G"Visual Studio 16" -A x64 .. 
+cmake -G"Visual Studio 16" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=G:/Dev/Programs/vcpkg/scripts/buildsystems/vcpkg.cmake .. 
 cmake --build . --config Release
 popd
