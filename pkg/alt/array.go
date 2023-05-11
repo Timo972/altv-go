a93ea924a29eefc9368b7c996db0aa2520f076d9
Timo <44401485+Timo972@users.noreply.github.com>
2023-05-11 10:59:00 +0200

    feat: convertArray free func

--- pkg/alt/array.go
+++ pkg/alt/array.go
@@ -11,23 +11,18 @@ import (
 
 // array converts an array struct to C Value slice
 // pointer is free'd afterwards
-func convertArray[V any](a C.struct_array) ([]V, int) {
+func convertArray[V any](a C.struct_array) ([]V, int, func()) {
 	size := int(a.size)
-	// FIXME: may causes a crash because it frees to early
-	// defer C.free(unsafe.Pointer(a.array))
-
 	cArray := (*[1 << 28]V)(a.array)[:size:size]
 
-	return cArray, size
+	return cArray, size, func() {
+		C.free(unsafe.Pointer(a.array))
+	}
 }
 
 func newStringArray(ptr unsafe.Pointer, size int) []string {
 	strings := make([]string, size)
 	cStrings := (*[1 << 28]*C.char)(ptr)[:size:size]
-
-	// for i := 0; i < size; i++ {
-	// 	strings[i] = C.GoString(cStrings[i])
-	// }
 
 	for i, cString := range cStrings {
 		strings[i] = C.GoString(cString)
