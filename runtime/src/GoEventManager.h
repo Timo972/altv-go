996128f3d26ac4d7bacbe34a53801de9541330eb
Timo <44401485+Timo972@users.noreply.github.com>
2023-09-03 13:31:13 +0200

    fix(runtime): only pass registered events

--- runtime/src/GoEventManager.h
+++ runtime/src/GoEventManager.h
@@ -12,13 +12,13 @@ namespace Go
 {
 class EventsManager : public Go::EventFactory
     {
-    //private:
-        //std::vector<alt::CEvent::Type> _registeredEvents;
+    private:
+        std::vector<alt::CEvent::Type> _registeredEvents;
     public:
         EventsManager() = default;
         bool RegisterEvent(alt::CEvent::Type type);
         bool UnregisterEvent(alt::CEvent::Type type);
-        //bool IsEventRegistered(alt::CEvent::Type type);
+        bool IsEventRegistered(alt::CEvent::Type type);
         void NotifyEvent(const alt::CEvent *ev, const std::string &resourceName);
     };
 }
